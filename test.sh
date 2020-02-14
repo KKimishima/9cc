bba5074dfb594473cca98313d2e8c411530cf7dd
KKimishima <k.kimishima123@gmail.com>
2020-02-14 22:41:48 +0900

    <追加>比較演算子追加

--- test.sh
+++ test.sh
@@ -2,19 +2,19 @@
 
 echo "test実行"
 try() {
-    exp="$1"
-    input="$2"
+  exp="$1"
+  input="$2"
 
-    ./9cc "$input" > tmp.s
-    gcc -o tmp tmp.s
-    ./tmp
-    act="$?"
+  ./9cc "$input" >tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  act="$?"
 
-    if [ "$act" = "$exp" ]; then
-        echo "テスト成功: $input => $act"
-    else
-        echo "テスト失敗: $input => $exp を期待しいましたが、 $act になっています"
-    fi
+  if [ "$act" = "$exp" ]; then
+    echo "テスト成功: $input => $act"
+  else
+    echo "テスト失敗: $input => $exp を期待しいましたが、 $act になっています"
+  fi
 }
 
 try 1 1
